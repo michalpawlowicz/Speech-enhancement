02b96b68cbc8fc608fd1239a42a6d47724f16979
Michał Pawlowicz <michal.pawlowicz@yahoo.com>
2020-04-13 23:22:03 +0200

    WIP

--- test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import librosa
-import scipy
-
-frame_size=8192
-X = np.random.uniform(1, 100, frame_size)
-
-print(X.shape)
-print(librosa.stft(X, n_fft=255, hop_length=64).shape)
--- train.py
+++ train.py
@@ -3,12 +3,16 @@ import os
 from model_unet import get_unet, unet
 import numpy as np
 import tensorflow as tf
+import time
 
 env = check_environment_variables(variables)
+
 X = np.load(os.path.join(env["SPECTROGRAM_TRAIN_NOISY"], "0.npy"))
 y = np.load(os.path.join(env["SPECTROGRAM_TRAIN_CLEAN"], "0.npy"))
+
 X = X.reshape(X.shape[0], X.shape[1], X.shape[2], 1)
 y = y.reshape(y.shape[0], y.shape[1], y.shape[2], 1)
+
 print(X.shape)
 print(y.shape)
 
@@ -16,5 +20,14 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.Session(config=config)
 
+# Save best models to disk during training
+checkpoint_name = "cp-{epoch:04d}.h5"
+checkpoint_path = os.path.join(env["CHECKPOINTS_DIR"], checkpoint_name)
+checkpoint = ModelCheckpoint(
+checkpoint_path, verbose=1, monitor='val_loss', save_best_only=True, mode='auto', period=1)
+log_dir = "logs/model-{}".format(int(time.time()))
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, update_freq='epoch', write_graph=True, profile_batch=0)
+
 model = unet()
-model.fit(X, y, epochs=10, batch_size=64, shuffle=True, callbacks=None, verbose=1)
+model.fit(X, y, epochs=10, batch_size=64,
+          shuffle=True, callbacks=None, verbose=1)
